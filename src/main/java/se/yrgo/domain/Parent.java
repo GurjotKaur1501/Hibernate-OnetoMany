019bc6aab5c703185cefef62085c92d82ec85463
GurjotKaur1501 <jotkau0115@gmail.com>
2025-03-19 10:38:44 +0100

    Create Parent.java

---
+++ src/main/java/se/yrgo/domain/Parent.java
@@ -0,0 +1,52 @@
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Child> children = new ArrayList<>();
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Child> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Child> children) {
+        this.children = children;
+    }
+
+    // Method to add a child
+    public void addChild(Child child) {
+        children.add(child);
+        child.setParent(this);
+    }
+
+    // Method to remove a child
+    public void removeChild(Child child) {
+        children.remove(child);
+        child.setParent(null);
+    }
+}
